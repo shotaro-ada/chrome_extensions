4c24604ac07925d5a2e73f8b72bbcee24fdd8d23
shotaro-ada <syouta2223505@gmail.com>
2022-10-14 16:27:59 +0900

    なおったかも

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
 'use strict';
 window.alert('プロ研が作った拡張機能！！')
 
-const imgup = () => {
-    let counts = document.querySelectorAll('d-flex flex-wrap');
 
-    counts.innerHTML = "<img src = 'images/proken_icon.png'>"
-}
+let counts = document.querySelectorAll('d-flex flex-wrap');
+
+counts.innerHTML = "<img src = 'images/proken_icon.png'>";
 
 
-window.addEventListener('load', () => imgup(), false);
+
