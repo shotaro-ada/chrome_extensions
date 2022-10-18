cea0d9f33285394f9eeb362b87fded1ac584be58
shotaro-ada <syouta2223505@gmail.com>
2022-10-18 12:33:10 +0900

    img srcのurl書き換えたらうまくいくと思う

--- content.js
+++ content.js
@@ -1,10 +1,30 @@
 'use strict';
-window.alert('プロ研が作った拡張機能！！')
+window.alert('日本の米は世界一')
+
+//画像表示
+function unko() {
+    document.getElementById("page-header").innerHTML = "<img src = './images/sushi.jpg'>";
+}
+
+window.onload = unko();
 
 
-let counts = document.querySelectorAll('d-flex flex-wrap');
-
-counts.innerHTML = "<img src = 'images/proken_icon.png'>";
 
 
 
+
+//文字を置き換える機能
+function f(n) {
+    let cs = n.childNodes;
+    for (let i = 0; i < cs.length; i++) {
+        let c = cs[i];
+        if (c.nodeType == Node.TEXT_NODE) {
+            c.textContent = c.textContent.replace(/Kyutech Moodle/g, "Kyutech Proken Moodle");
+        }
+        else {
+            f(c);
+        }
+    }
+}
+
+window.onload = f(document.body);
